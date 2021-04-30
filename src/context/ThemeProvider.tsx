b5d747e24413adba4b7164722df2f4657c0ae224
AdrianApan <mail@adrianapan.com>
2021-04-30 15:38:05 +0100

    ✨ add prefers-color-scheme check

--- src/context/ThemeProvider.tsx
+++ src/context/ThemeProvider.tsx
@@ -1,5 +1,5 @@
-import React, { useCallback, useEffect, useState } from 'react'
-import { Theme, MuiThemeProvider, CssBaseline } from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
+import { MuiThemeProvider, CssBaseline } from '@material-ui/core'
 
 import darkTheme from '../themes/dark'
 import lightTheme from '../themes/light'
@@ -13,44 +13,47 @@ export const ToggleThemeContext = React.createContext({
   isDark: false,
 })
 
-type SelectedTheme = {
-  themeName: string
-  appliedTheme: Theme
-}
-
 export const ThemeProvider: React.FC<ThemeProviderProps> = ({
   children,
 }: ThemeProviderProps) => {
-  const [selectedTheme, setSelectedTheme] = useState<SelectedTheme>({
-    appliedTheme: lightTheme,
-    themeName: 'lightTheme',
-  })
+  const [themePalette, setThemePalette] = useState<string | null>(null)
 
   useEffect(() => {
-    const theme = localStorage.getItem('theme')
-    if (theme && theme === 'lightTheme') {
-      setSelectedTheme({ appliedTheme: lightTheme, themeName: 'lightTheme' })
+    if (typeof window !== 'undefined') {
+      const localTheme = window.localStorage.getItem('theme')
+      if (!localTheme) {
+        setThemePalette(
+          window.matchMedia &&
+            window.matchMedia('(prefers-color-scheme: dark)').matches
+            ? 'darkTheme'
+            : 'lightTheme'
+        )
+      } else {
+        setThemePalette(localTheme)
+      }
     }
   }, [])
 
-  const toggleTheme = useCallback(() => {
-    if (!selectedTheme || selectedTheme.themeName === 'darkTheme') {
-      setSelectedTheme({ appliedTheme: lightTheme, themeName: 'lightTheme' })
-      localStorage.setItem('theme', 'lightTheme')
-    } else {
-      setSelectedTheme({ appliedTheme: darkTheme, themeName: 'darkTheme' })
-      localStorage.setItem('theme', 'darkTheme')
-    }
-  }, [selectedTheme, setSelectedTheme])
+  function toggleTheme() {
+    setThemePalette((prev) =>
+      prev === 'lightTheme' ? 'darkTheme' : 'lightTheme'
+    )
+    window.localStorage.setItem(
+      'theme',
+      themePalette === 'lightTheme' ? 'darkTheme' : 'lightTheme'
+    )
+  }
 
   return (
     <ToggleThemeContext.Provider
       value={{
         toggleTheme,
-        isDark: !(selectedTheme.themeName === 'lightTheme'),
+        isDark: !(themePalette === 'lightTheme'),
       }}
     >
-      <MuiThemeProvider theme={selectedTheme.appliedTheme}>
+      <MuiThemeProvider
+        theme={themePalette === 'darkTheme' ? darkTheme : lightTheme}
+      >
         <CssBaseline />
         {children}
       </MuiThemeProvider>
