0f12779b5700c2be4c213930fff1219ebd58e9e5
Adrian Apan <mail@adrianapan.com>
2024-06-07 09:58:20 +0100

    💄 adjust hero height

--- app/components/Hero/Home/HomeHero.tsx
+++ app/components/Hero/Home/HomeHero.tsx
@@ -10,7 +10,7 @@ const HomeHero = () => {
 
   return (
     <div className="hero">
-      <div className="hero-content h-full md:h-screen max-w-screen-lg p-8 py-12">
+      <div className="hero-content h-full md:h-[calc(100vh-140px)] max-w-screen-lg p-8 py-12">
         <div className="max-w-full">
           <div className="text-center text-4xl font-semibold md:text-5xl text-black dark:text-white mb-2 leading-12 md:leading-14">
             {animatedContent}
